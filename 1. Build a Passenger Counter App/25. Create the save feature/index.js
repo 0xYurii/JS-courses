8eac48d5f07dd377f407a451b63b61c22cb4d618
Younes Heabaiche <haitemhebaiche08@gmail.com>
2025-08-09 21:02:11 +0100

    Refactor increment function to use shorthand syntax and implement save feature to display previous entries

--- 1. Build a Passenger Counter App/25. Create the save feature/index.js
+++ 1. Build a Passenger Counter App/25. Create the save feature/index.js
@@ -1,7 +1,9 @@
 // 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl= document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+console.log(saveEl)
 function increment() {
     count += 1
     countEl.innerText = count
@@ -9,8 +11,15 @@ function increment() {
 
 function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+
+
+    let contains= " "+count+" - "
     // 3. Render the variable in the saveEl using innerText
+
+    saveEl.innerText+=contains
     // NB: Make sure to not delete the existing content of the paragraph
+    
+    console.log(contains)
     console.log(count)
 }
 
