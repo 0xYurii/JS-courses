351be89cade73f08301680ebe392abeb8860d4c6
Younes Heabaiche <haitemhebaiche08@gmail.com>
2025-08-10 11:44:45 +0100

    Update Blackjack game logic with card values and conditional statements

--- 3. Build a Blackjack game/03. If...else conditionals/index.js
+++ 3. Build a Blackjack game/03. If...else conditionals/index.js
@@ -1,4 +1,11 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 11
+let secondCard = 10
 let sum = firstCard + secondCard
 
+if (sum<21) {
+    console.log("do you wanna draw new card? ")
+} else if (sum===21){
+    console.log("Whoo! you got blackjack 🥳")
+} else {
+    console.log("You're out of the game 😔")
+}
