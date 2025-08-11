6ccaff2a08e8b444045c60b1935213e802b1f01b
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-11 23:58:30 +0100

    Add input field and button; implement myLeads array and unordered list

--- 5. Build a Chrome Extension/07. Your turn to refactor/index.js
+++ 5. Build a Chrome Extension/07. Your turn to refactor/index.js
@@ -1,9 +1,12 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
-function saveLead() {
+// function saveLead() {
+//     console.log("Button clicked!")
+// }
+
+let input=document.getElementById("input-btn")
+
+input.addEventListener("click",function(){
     console.log("Button clicked!")
-}
-
-
-
+})
