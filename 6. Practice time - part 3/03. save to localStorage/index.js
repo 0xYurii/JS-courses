035aa40b4c50e6c0ee9820b1e547d90c31ac6fa3
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-12 22:24:41 +0100

    Refactor variable declarations and implement array logging with localStorage and event listener

--- 6. Practice time - part 3/03. save to localStorage/index.js
+++ 6. Practice time - part 3/03. save to localStorage/index.js
@@ -1,3 +1,9 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+
+
+
+// localStorage.setItem("myskill","i love you")
+
+console.log(localStorage.getItem("myskill"))
